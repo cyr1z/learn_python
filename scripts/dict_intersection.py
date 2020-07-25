90f9f5b6dd7715576ed67f13aeab0d3a5ff6add5
cyr <cyr@zolotarev.pp.ua>
2020-07-25 09:24:44 +0300

    add comments

--- scripts/dict_intersection.py
+++ scripts/dict_intersection.py
@@ -9,10 +9,11 @@ def dict_intersection(a, b, confirm_values=True):
     if confirm_values:
         return dict(filter(lambda e: e[0] in a.keys() & b.keys() and a[e[0]] == b[e[0]], d.items()))
     return dict(filter(lambda e: e[0] in a.keys() & b.keys(), d.items()))
-
+ 
 
 if __name__ == '__main__':
     dict_a = {1: 2, 3: 6, 4: 9, "t": [7, 6, 4]}
     dict_b = {3: 6, 7: 2, 4: 5, "t": [7, 6, 4]}
-    print(dict_intersection(dict_a, dict_b))
-    print(dict_intersection(dict_a, dict_b, confirm_values=False))
+    print(dict_intersection(dict_a, dict_b))    #  {3: 6, 't': [7, 6, 4]}
+    print(dict_intersection(dict_a, dict_b, confirm_values=False))  # {3: 6, 4: 5, 't': [7, 6, 4]}
+ 

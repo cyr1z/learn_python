5a90f5dc01e71774b2ed54fc81bf13a1c37a6db5
cyr <cyr@zolotarev.pp.ua>
2020-08-23 18:08:38 +0300

    int to roman

--- homeworks/hw_op_1/class_to_convert_an_integer_to_a_roman_numeral.py
+++ homeworks/hw_op_1/class_to_convert_an_integer_to_a_roman_numeral.py
@@ -8,10 +8,9 @@ class IntegerToRoman:
     def __init__(self, num):
         try:
             self.integer = int(num)
-            if self.integer > 3999:
-                print('wrong integer')
         except ValueError:
             print('wrong integer')
+            exit()
 
     @staticmethod
     def __get_roman_notation__(x):
@@ -35,6 +34,8 @@ class IntegerToRoman:
         :return: Roman numeral string('CLIX')
         """
         roman_registers = (('I', 'V', 'X'), ('X', 'L', 'C'), ('C', 'D', 'M'), ('M',))
+        if self.integer > 3999:
+            return 'bigger number'
         x = str(self.integer)
         x_register_list = tuple(map(int, x.strip()))
         pair_registers = tuple(zip(x_register_list[::-1], roman_registers))
@@ -51,7 +52,10 @@ if __name__ == '__main__':
     a = IntegerToRoman(10)
     b = IntegerToRoman(312)
     c = 2014
+    d = 3999
 
-    print(f'{a.integer} is roman {a}')
-    print(f'{b.integer} is roman {b}')
-    print(f'{c} is roman {IntegerToRoman(c).get_roman()}')
+    print(f'{a.integer} is roman {a}')  # 10 is roman X
+    print(f'{b.integer} is roman {b}')  # 312 is roman CCCXII
+    print(f'{c} is roman {IntegerToRoman(c).get_roman()}')  # 2014 is roman MMXIV
+    print(f'{d} is roman {IntegerToRoman(d)}')  # 3999 is roman MMMCMXCIX
+    print(f'{4000} is roman {IntegerToRoman(4000).get_roman()}')  # 4000 is roman bigger number

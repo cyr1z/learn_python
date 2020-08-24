c0ae11042e43b6cf64c5da45d2cd502b2f3bf56f
cyr <cyr@zolotarev.pp.ua>
2020-08-25 00:45:27 +0300

    #9 set string and print string upper

--- homeworks/hw_op_1/rewerse_string.py
+++ homeworks/hw_op_1/rewerse_string.py
@@ -1,9 +1,10 @@
 class ReverseWords:
-    def __init__(self,string):
+    def __init__(self, string):
         self.string = string
         self.reverse = ' '.join(reversed(string.split()))
-        
+
     def __str__(self):
         return self.reverse
+
 
 print(ReverseWords('Happy New year'))

7fc1c2ed81d37f4166ea62fe6f442d2b3abd1232
cyr <cyr@zolotarev.pp.ua>
2020-08-24 17:30:34 +0300

    subsets

---
+++ homeworks/hw_op_1/subsets.py
@@ -0,0 +1,21 @@
+class SubSets:
+    @staticmethod
+    def subsets(arr):
+        result = []
+        for i in range(2 ** len(arr)):
+            subs = [arr[j] for j in range(len(arr)) if not i & (2**j)]
+            result.append(subs)
+        return result
+
+    def sub_sets(self, s_set):
+        return self.subsets_recur([], s_set)
+
+    def subsets_recur(self, current, s_set):
+        if s_set:
+            return self.subsets_recur(current, s_set[1:]) + self.subsets_recur(current + [s_set[0]], s_set[1:])
+        return [current]
+
+
+d = [1, 2, 3, 5]
+print(SubSets().subsets(d))
+print(SubSets().sub_sets(d))

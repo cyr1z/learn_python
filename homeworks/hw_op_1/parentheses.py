af3a3e68623bef12ea6a301db73367c7ccb58bed
cyr <cyr@zolotarev.pp.ua>
2020-08-24 11:25:06 +0300

    parentheses

---
+++ homeworks/hw_op_1/parentheses.py
@@ -0,0 +1,26 @@
+class Parentheses:
+    open_list = ["[", "{", "("]
+    close_list = ["]", "}", ")"]
+
+    def is_parentheses(self, string):
+        stack = []
+        for i in string:
+            if i in self.open_list:
+                stack.append(i)
+            if i in self.close_list:
+                if self.close_list.index(i) == self.open_list.index(stack[-1]):
+                    stack.pop()
+                else:
+                    return False
+        if stack:
+            return False
+        return True
+
+
+balanced_string = '123 (14) [2, 3: {16, 9}, (90, a[1])]'
+unbalanced_string = '44 (5t6y) [2, 3: {16, 9}, {(90, a[1]))]'
+
+print(Parentheses().is_parentheses(balanced_string))  # True
+print(Parentheses().is_parentheses(unbalanced_string))  # False
+print(Parentheses().is_parentheses('{}'))   # True
+print(Parentheses().is_parentheses('{'))  # False

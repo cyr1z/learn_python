10a27b867db8a3735d19a19cb6f8f0d2ed64e8d0
cyr <cyr@zolotarev.pp.ua>
2020-08-25 14:32:42 +0300

    #9 class with two methods get_String and print_String

--- homeworks/hw_op_1/get_string_upper.py
+++ homeworks/hw_op_1/get_string_upper.py
@@ -1,3 +1,7 @@
+# 9. Write a Python class which has two methods get_String and print_String.
+# get_String accept a string from the user and print_String print the string in upper case.
+
+
 class StringInOut:
     string = ''
 

5ac6efdb3078fa5d3c41399814af8cffebcf4173
cyr <cyr@zolotarev.pp.ua>
2020-08-25 14:18:49 +0300

    #11 class named Circle constructed by a radius

--- homeworks/hw_op_1/round.py
+++ homeworks/hw_op_1/round.py
@@ -1,4 +1,8 @@
-class Round:
+# 11. Write a Python class named Circle constructed by a radius and two methods
+# which will compute the area and the perimeter of a circle.
+
+
+class Circle:
     def __init__(self, radius):
         self.radius = radius
 
@@ -9,5 +13,5 @@ class Round:
         return self.radius*2*3.14
 
 
-circle = Round(6)
+circle = Circle(6)
 print(circle.get_area(), circle.get_perimeter())

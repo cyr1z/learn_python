8d6019c7d67a5454a302881441d195a45c40831a
cyr <cyr@zolotarev.pp.ua>
2020-08-25 14:13:22 +0300

    #10 class rectangle with area

--- homeworks/hw_op_1/rectangle.py
+++ homeworks/hw_op_1/rectangle.py
@@ -1,3 +1,7 @@
+# 10. Write a Python class named Rectangle constructed by a length and
+# width and a method which will compute the area of a rectangle.
+
+
 class Rectangle:
     def __init__(self, length, width):
         self.length = length
